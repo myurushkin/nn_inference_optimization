45874c4dd2cf8ace3bbf000002660b86dee61d03
misha <m.yurushkin@gmail.com>
2022-07-15 18:18:01 +0300

    bugfixes

--- README.md
+++ README.md
@@ -4,13 +4,14 @@ Experements that help to check the real performance of neural network inference 
 
 Parameters:
  * gpu: gtx1070
- * nn: simple_network | yolov5x (default)
+ * nn: simple_network | yolov5x (default) | resnet50
 
 Report generated for default settings:
 
 ```
 NN GFLOPs: 77.1287745
 GPU peak performance (GFLOPS): 6500.0
-Average NN execution time (sec): 0.03834035396575928
+Average NN execution time (sec): 0.04161948627895779
 Theoretical minimal inference time (sec): 0.011865965307692308
+Inference relative performance 28.51%
 ```
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from torch import nn
 from ptflops import get_model_complexity_info
 import numpy as np
 from omegaconf import DictConfig, OmegaConf
+import torchvision.models as models
 
 
 class NeuralNetwork(nn.Module):
@@ -27,26 +28,32 @@ def measure(model, input):
 
 @hydra.main(version_base=None, config_path="conf", config_name="config")
 def my_app(cfg : DictConfig) -> None:
+    print("----------------------------")
+    print("nn.name: {0}".format(cfg.nn.name))
+    print("gpu.TFLOPS: {0}".format(cfg.gpu.TFLOPS))
+
+    batch_size = 32
     with torch.cuda.device(0):
         if cfg.nn.name == 'simple_network':
             net = NeuralNetwork()
             input_tensor_shape = (1, 100)
             input = np.zeros(input_tensor_shape, dtype=np.float32)
         if cfg.nn.name == 'yolov5x':
-            net = torch.hub.load('ultralytics/yolov5', 'yolov5x', pretrained=True)
+            net = torch.hub.load('ultralytics/yolov5', 'yolov5x', pretrained=True, verbose=False)
             input_tensor_shape = (3, 640, 480)
             input = np.expand_dims(np.zeros(input_tensor_shape, dtype=np.float32), 0)
+        if cfg.nn.name == 'resnet50':
+            net = models.resnet50()
+            input_tensor_shape = (3, 224, 224)
+            input = np.expand_dims(np.zeros(input_tensor_shape, dtype=np.float32), 0)
 
+        net = net.cuda()
+        input = torch.tensor(input).cuda()
+        results = [measure(net, input) for _ in range(cfg.experiments_count)]
+        everage_time = np.mean(results[1:])
 
         macs, params = get_model_complexity_info(net, input_tensor_shape, as_strings=False,
-                                                 print_per_layer_stat=False, verbose=True)
-
-        input = torch.tensor(input)
-
-
-
-        results = [measure(net, input) for _ in range(cfg.experiments_count)]
-        everage_time = np.mean(results)
+                                                 print_per_layer_stat=False, verbose=False)
 
     gpuPeakPerformance = cfg.gpu.TFLOPS * 1e12
     theoretical_nn_inference_time = macs / gpuPeakPerformance
@@ -55,6 +62,7 @@ def my_app(cfg : DictConfig) -> None:
     print("GPU peak performance (GFLOPS): {0}".format(gpuPeakPerformance / 1e9))
     print("Average NN execution time (sec): {0}".format(everage_time))
     print("Theoretical minimal inference time (sec): {0}".format(theoretical_nn_inference_time))
+    print("Inference relative performance {0:.2f}%".format(theoretical_nn_inference_time / everage_time * 100))
 
 
 if __name__ == "__main__":
